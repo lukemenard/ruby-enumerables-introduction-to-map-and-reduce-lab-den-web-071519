15373066f00170ede3b862d708138b7767c53da0
Luke Menard <lukemenard@gmail.com>
2019-07-06 21:57:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,13 @@ def map_to_negativize(source_array)
   end
   return new_array
 end
+
+def map_to_no_change(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    new_array << source_array[i] * 1
+    i += 1
+  end
+  return new_array
+end
