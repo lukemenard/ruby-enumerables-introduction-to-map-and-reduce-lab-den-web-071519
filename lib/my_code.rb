a5a3dd8542e37040387848974261f339e8c498b9
Luke Menard <lukemenard@gmail.com>
2019-07-06 22:44:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,3 +58,15 @@ def reduce_to_all_true(source_array)
   end
   return new_value
 end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] == true
+      new_value = true
+    else new_value = false
+    end
+    i += 1
+  end
+  return new_value
+end
