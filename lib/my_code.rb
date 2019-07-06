4f9b17e05aa080073266a7989c283c0379528f5a
Luke Menard <lukemenard@gmail.com>
2019-07-06 22:08:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,10 +38,11 @@ def map_to_square(source_array)
   return new_array
 end
 
-def reduce_to_total(source_array, starting_point)
+
+def reduce_to_total(source_array)
   i = 0
   while i < source_array.length do
-    new_value = starting_point += source_array[i]
+    new_value += source_array[i]
     i += 1
   end
   return new_value
