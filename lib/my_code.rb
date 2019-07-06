fc0e48379cfeb4ae063f2fbd768f52af745e8a28
Luke Menard <lukemenard@gmail.com>
2019-07-06 22:00:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,7 +42,7 @@ def reduce_to_total(source_array)
   i = 0
   new_array = []
   while i < source_array.length do
-    new_array + source_array[i] ** 2
+    new_array + source_array[i]
     i += 1
   end
   return new_array
